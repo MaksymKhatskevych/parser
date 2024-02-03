cd47f9d3e2931c71d3e68cbaba0521e1affc2cb3
Maks <74023131+MaksymKhatskevych@users.noreply.github.com>
2024-02-03 10:44:15 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -25,69 +25,66 @@ def num_phones(soup, url):
         print('Error during the request:', response.status_code)
         return []
 
+def extract_car_data(car_url):
+    response = requests.get(car_url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'lxml')
+
+        car_data = {}
+        car_title_element = soup.find('h3', class_='auto-content_title')
+        car_data['car_title'] = car_title_element.text.split() if car_title_element else ["N/A"]
+
+        price_element = soup.find('span', class_='price_value')
+        car_data['price'] = price_element.text.split() if price_element else ["N/A"]
+
+        car_number_element = soup.find('span', class_='state-num ua')
+        car_data['car_number'] = (car_number_element.text.strip()
+                                  .replace("Ми розпізнали держномер авто на фото та перевірили його за реєстрами МВС",
+                                           "")) if car_number_element else "N/A"
+
+        username_element = soup.find('div', class_='seller_info_name')
+        car_data['username'] = username_element.text.strip() if username_element else "N/A"
+
+        vin_code_element = soup.find('span', class_='label-vin')
+        car_data['vin_code'] = vin_code_element.text if vin_code_element else "N/A"
+
+        image_element = soup.find('img', class_='outline m-auto')
+        car_data['image_link'] = image_element.get('src') if image_element else "N/A"
+
+        car_data['phones'] = num_phones(soup, car_url)
+
+        return car_data
+    else:
+        print(f'Error during the request for {car_url}: {response.status_code}')
+        return None
+
+
 def scrape_page(url, max_pages=10):
     print('Start_______' * 30)
     count = 0
     cars = []
 
-    for page in range(1, max_pages+1):
+    for page in range(1, max_pages + 1):
         print(f"Processing page {page}")
         response = requests.get(url)
-        html_doc = response.text
-        soup = BeautifulSoup(html_doc, 'lxml')
+        if response.status_code != 200:
+            print(f'Error during the request for {url}: {response.status_code}')
+            break
 
+        soup = BeautifulSoup(response.text, 'lxml')
         car_cards = soup.find_all('section', class_='ticket-item')
 
         for car_card in car_cards:
-            car_data = {}
             url_car = car_card.find('a', class_='m-link-ticket')
             if url_car:
                 car_url = url_car.get('href')
                 print(f"Processing car: {car_url}")
 
-                car_response = requests.get(car_url)
-                car_html_doc = car_response.text
-                car_soup = BeautifulSoup(car_html_doc, 'lxml')
-
-                car_title = car_soup.find('h3', class_='auto-content_title')
-                if car_title:
-                    car_data['car_title'] = car_title.text.split()
-
-                price = car_soup.find('span', class_='price_value')
-                if price:
-                    car_data['price'] = price.text.split()
-
-                car_number_element = car_soup.find('span', class_='state-num ua')
-                if car_number_element:
-                    car_number_text = car_number_element.text.strip().replace("Ми розпізнали держномер авто на фото та перевірили його за реєстрами МВС", "")
-                    car_data['car_number'] = car_number_text.strip()
-
-                seller_info_area = car_soup.find('div', class_='seller_info_area')
-                if seller_info_area:
-                    username = seller_info_area.find('div', class_='seller_info_name')
-                    if username:
-                        car_data['username'] = username.text.strip()
-                    else:
-                        car_data['username'] = "N/A"
-                else:
-                    car_data['username'] = "N/A"
-
-                vin_code = car_soup.find('span', class_='label-vin')
-                if vin_code:
-                    car_data['vin_code'] = vin_code.text
-
-                image = car_soup.find('img', class_='outline m-auto')
-                if image:
-                    image_link = image.get('src')
-                    if image_link:
-                        car_data['image_link'] = image_link
-
-                phones = num_phones(car_soup, car_url)
-                car_data['phones'] = phones
-
-                print(car_data)
-                cars.append(car_data)
-                count += 1
+                car_data = extract_car_data(car_url)
+                if car_data:
+                    print(car_data)
+                    cars.append(car_data)
+                    count += 1
 
         next_page = soup.find("a", class_="page-link js-next")
         if next_page:
